40b9876fe10bdebc80004d8e6cc20fda8ee65ebe
Zoe Arbuckle <zoe.arbuckle@me.com>
2020-06-03 16:05:14 -0400

    Initial Commit

---
+++ JapanesePronunciationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JapanesePronunciationApp
+//
+//  Created by Zoe Arbuckle on 6/3/20.
+//  Copyright © 2020 Zoe Arbuckle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
